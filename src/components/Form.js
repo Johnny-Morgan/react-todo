a21069e484921f83842f882474e153144804ba71
John Morgan <jwjmorgan@gmail.com>
2021-12-28 20:22:30 +0000

    Add inputTextHandler function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ inputText, setInputText }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <form>
-      <input type="text" className="todo-input" />
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
       <button className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
