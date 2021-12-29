58026f0490bb1bd89223e0db15ebefa5022cdf6f
John Morgan <jwjmorgan@gmail.com>
2021-12-29 22:59:44 +0000

    Style Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,11 +3,11 @@ import React from "react";
 const Todo = () => {
   return (
     <div className="todo">
-      <li className="todo-item"></li>
-      <button>
+      <li className="todo-item">Hello</li>
+      <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button>
+      <button className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
